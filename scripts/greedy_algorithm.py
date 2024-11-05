c4d639bdedba419d35523534abb4a5efecc5a674
ishitachakravarthy <ishitac1150@gmail.com>
2024-11-05 12:22:55 -0500

    Greedy update to matrices

--- scripts/greedy_algorithm.py
+++ scripts/greedy_algorithm.py
@@ -1,66 +1,76 @@
-items = ['o1','o2','o3','o4','o5','o6','o7','o8','o9']
-agents = [1,2,3]
-c=2
+import numpy as np
 
-X_c={
-    0:set(['o1', 'o2',  'o7', 'o8', 'o9']),
-    1:set(['o3', 'o4', 'o6']),
-    2:set(['o5']),
-    3:set()
-}
-X_0={
-    0:set(['o3','o4' 'o5', 'o6', 'o7', 'o8', 'o9']),
-    1:set(['o1', 'o2']),
-    2:set(),
-    3:set([])
-}
-X__1={
-    0:set(['o7', 'o8', 'o9']),
-    1:set(),
-    2:set(),
-    3:set()
-}
+items = ["o1", "o2", "o3", "o4", "o5", "o6", "o7", "o8", "o9"]
+agents = [1, 2, 3]
+c = 3
 
-# Print utilities of agents after before allocation is done
-for agent in agents:
-        value= 0*len(X_0[agent])+(c*len(X_c[agent]) )-len(X__1[agent])
-        print(f"Agent {agent}: {value}")
+X_c_matr = initialize_allocation_matrix(items, agents)
+X_0_matr = initialize_allocation_matrix(items, agents)
+X__1_matr = initialize_allocation_matrix(items, agents)
 
-def allocate_remaining_items(X_c, X_0, X__1):
-    # While there is an element in the set intersection
-    while set(X_c[0]) & set(X_0[0]) & set(X__1[0]) :
+# Set example allocation for testing
+X_c_matr[:, 0] = [0, 0, 1, 1, 0, 1, 0, 0, 0]
+X_c_matr[:, 1] = [0, 0, 0, 0, 1, 0, 0, 0, 0]
+X_c_matr[:, 3] = [1, 1, 0, 0, 0, 0, 1, 1, 1]
 
+X_0_matr[:, 0] = [1, 1, 0, 0, 0, 0, 0, 0, 0]
+X_0_matr[:, 3] = [0, 0, 1, 1, 1, 1, 1, 1, 1]
+
+
+def find_current_utilities(agent, X_c_matr, X_0_matr, X__1_matr):
+    #  Given an agent find the utility of the agent
+    value = (
+        np.sum(X_c_matr[:, agent]) * c
+        + np.sum(X_0_matr[:, agent]) * 0
+        - np.sum(X__1_matr[:, agent])
+    )
+    return value
+
+
+def find_max_utility_agent(agents, X_c_matr, X_0_matr, X__1_matr):
+    max_agent = None
+    max_value = -float("inf")
+    for agent in range(len(agents)):
+        value = find_current_utilities(agent, X_c_matr, X_0_matr, X__1_matr)
+        if value >= max_value:
+            max_value = value
+            max_agent = agent
+    return max_agent, max_value
+
+
+def allocate_remaining_items_matr(X_c_matr, X_0_matr, X__1_matr):
+
+    # Create a list of unallocated items
+    unallocated_items_arr = X_c_matr[:, -1] & X_0_matr[:, -1] & X__1_matr[:, -1]
+    unallocated_items = list((unallocated_items_arr > 0).nonzero()[0])
+
+    # While there are unallocated items
+    while unallocated_items:
         # Find the agent with maximum utility with maximum index
-        max_agent = None
-        max_value = -float('inf')
-        for agent in agents:
-            value= 0*len(X_0[agent])+(c*len(X_c[agent]) )-len(X__1[agent])
-            if value >= max_value:
-                max_value = value
-                max_agent = agent
+        max_agent, max_value = find_max_utility_agent(
+            agents, X_c_matr, X_0_matr, X__1_matr
+        )
 
-        # Pick an arbitrary item from the set intersection
-        unallocated_items= set(X_c[0]) & set(X_0[0]) & set(X__1[0])
+        # Pick and remove an arbitrary item from the unallocated items
         item = unallocated_items.pop()
 
         # Assign an arbitrary unallocated item to the max utility agent
-        X__1[max_agent].add(item)
+        X__1_matr[item, max_agent] = 1
 
         # Remove assigned item from set of unallocated items
-        X__1[0].remove(item)
-    return X_c, X_0, X__1
+        X__1_matr[item, -1] = 0
+    return X_c_matr, X_0_matr, X__1_matr
 
 
+# Print utilities of agents before allocation is done
+for agent in range(len(agents)):
+    value = find_current_utilities(agent, X_c_matr, X_0_matr, X__1_matr)
+    print(f"Value of agent id {agent}: {value}")
 
-X_c, X_0, X__1= allocate_remaining_items(X_c, X_0, X__1)
-
-print(f"Xc: {X_c}")
-print(f"X0: {X_0}")
-print(f"X__1: {X__1}")
-
-# Print utilities of agents after all allocation is done
-for agent in range(1, len(agents)+1):
-        value= 0*len(X_0[agent])+(c*len(X_c[agent]) )-len(X__1[agent])
-        print(f"Agent {agent}: {value}")
-
-
+X_c_matr, X_0_matr, X__1_matr = allocate_remaining_items_matr(
+    X_c_matr, X_0_matr, X__1_matr
+)
+print("New matrices after allocation: ")
+print(f"Xc: {X_c_matr}")
+print(f"X0: {X_0_matr}")
+print(f"X__1: {X__1_matr}")
